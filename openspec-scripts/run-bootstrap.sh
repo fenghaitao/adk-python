249689c4b371303a91fe27131f6984792a0fe559
Li Wang <li.w.wang@intel.com>
2025-12-14 19:44:25 -0800

    Update adk-python path in run-bootstrap.sh
    Signed-off-by: Li Wang <li.w.wang@intel.com>

--- openspec-scripts/run-bootstrap.sh
+++ openspec-scripts/run-bootstrap.sh
@@ -3,7 +3,7 @@
 # Source common configuration
 source "$(dirname "$0")/common-config.sh"
 
-~/adk-python/run_openspec.sh \
+$ADK_ROOT/run_openspec.sh \
   adk_openspec_project \
   --model iflow/qwen3-coder-plus \
   --port 8056
