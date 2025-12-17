5247307ef83af7eaa6732922f00b711c50f60f18
Li Wang <li.w.wang@intel.com>
2025-12-16 18:11:25 -0800

    Update adk-python path used in openspec scripts
    Signed-off-by: Li Wang <li.w.wang@intel.com>

--- openspec-scripts/common-config.sh
+++ openspec-scripts/common-config.sh
@@ -7,6 +7,7 @@ export CONTEXT_KEEP_SYSTEM_MESSAGES="2"
 export CONTEXT_KEEP_RECENT_TURNS="3"
 export CONTEXT_SUMMARIZATION_MODEL="iflow/qwen3-coder-plus"
 export CONTEXT_SUMMARY_PROMPT_TYPE="vscode"
-export ADK_ROOT=~/adk-python
+export ADK_ROOT=$(realpath "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")
 export BUILTIN_MCP_SERVER=no
 export TMPDIR=~/tmp
+echo "ADK_ROOT is set to $ADK_ROOT"
