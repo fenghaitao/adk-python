c63d4fa7181818c66051f03908c7aa554cc46dff
Li Wang <li.w.wang@intel.com>
2025-12-18 22:11:11 -0800

    Add argument 2 as change_id in run-openspec-apply.sh
    Signed-off-by: Li Wang <li.w.wang@intel.com>

--- openspec-scripts/run-openspec-apply.sh
+++ openspec-scripts/run-openspec-apply.sh
@@ -5,10 +5,11 @@ source "$(dirname "$0")/common-config.sh"
 
 # Use provided workdir or default to adk_openspec_project
 WORKDIR=${1:-adk_openspec_project}
+CHANGE_ID=${2:-implement-wdt-initial}
 
 $ADK_ROOT/openspec-scripts/run_openspec_subagents.sh \
   --workdir $WORKDIR \
   --port 8056 \
   --apply \
-  --change-id implement-wdt-initial \
+  --change-id $CHANGE_ID \
   --model iflow/qwen3-coder-plus
