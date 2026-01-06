cf333d32e4f66c48f5e77bf496b226cdb71a211c
Haitao Feng <haitao.feng@gmail.com>
2026-01-06 01:12:19 -0800

    Cleanup qodercli

--- experiments/qodercli/adk_openspec_agent/agent.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add samples directory to path first for package imports
-sys.path.insert(0, '/home/hfeng1/adk-python/contributing/samples')
-
-# Import the OpenSpec agent as a package module (enables relative imports)
-from openspec_integration.agent import root_agent
